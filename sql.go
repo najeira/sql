e26c12440a6c3bdf76dc00f0106226e57d63cdf0
najeira <najeira@gmail.com>
2015-11-20 16:38:44 +0900

    separate files

--- sql.go
+++ sql.go
@@ -66,29 +66,3 @@ func (d *Db) RunInTx(f func(DB) error) error {
 	}
 	return err
 }
-
-type Tx struct {
-	*sql.Tx
-}
-
-var _ DB = (*Tx)(nil)
-
-func (t *Tx) Query(s Scanner, q string, args ...interface{}) ([]Row, error) {
-	return sqlQuery(t.Tx, s, q, args...)
-}
-
-func (t *Tx) QueryAsync(s Scanner, q string, args ...interface{}) chan QueryResult {
-	return sqlQueryAsync(t.Tx, s, q, args...)
-}
-
-func (t *Tx) Exec(q string, args ...interface{}) (int64, int64, error) {
-	return sqlExec(t.Tx, q, args...)
-}
-
-func (t *Tx) ExecAsync(q string, args ...interface{}) chan ExecResult {
-	return sqlExecAsync(t.Tx, q, args...)
-}
-
-func (t *Tx) RunInTx(f func(DB) error) error {
-	return f(t)
-}
